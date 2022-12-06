0ce9c1f67a5497d6c2bdfb9dc19fed9a3943abf0
ByrMucahit <m.mucahitbayar@gmail.com>
2022-12-06 22:27:09 +0300

    Initial commit

---
+++ src/main/java/com/example/arasanainterview/ArasanaInterviewApplication.java
@@ -0,0 +1,13 @@
+package com.example.arasanainterview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArasanaInterviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArasanaInterviewApplication.class, args);
+	}
+
+}
