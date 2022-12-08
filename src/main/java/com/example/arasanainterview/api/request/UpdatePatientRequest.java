9106321bf46ee76dbe17a8fbee37faa40f511101
ByrMucahit <m.mucahitbayar@gmail.com>
2022-12-08 14:03:56 +0300

    Controller Tests have been written.

--- src/main/java/com/example/arasanainterview/api/request/UpdatePatientRequest.java
+++ src/main/java/com/example/arasanainterview/api/request/UpdatePatientRequest.java
@@ -1,8 +1,10 @@
 package com.example.arasanainterview.api.request;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class UpdatePatientRequest {
     private String name;
     private String surname;
--- src/test/java/com/example/arasanainterview/api/controller/PatientControllerTest.java
+++ src/test/java/com/example/arasanainterview/api/controller/PatientControllerTest.java
@@ -1,35 +1,34 @@
 package com.example.arasanainterview.api.controller;
 
-import ch.qos.logback.core.net.ObjectWriter;
 import com.example.arasanainterview.api.request.SavePatientRequest;
+import com.example.arasanainterview.api.request.UpdatePatientRequest;
+import com.example.arasanainterview.domain.PatientUser;
 import com.example.arasanainterview.service.patient.PatientService;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
 import org.junit.jupiter.api.Test;
-import static org.mockito.BDDMockito.given;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import java.nio.charset.Charset;
+import java.util.Collections;
 
-import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8;
-import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(controllers = PatientContoller.class)
 @AutoConfigureMockMvc(addFilters = false)
 public class PatientControllerTest {
-    public static final MediaType APPLICATION_JSON_UTF8 = new MediaType(MediaType.APPLICATION_JSON.getType(), MediaType.APPLICATION_JSON.getSubtype(), Charset.forName("utf8"));
 
     @Autowired
     private MockMvc mockMvc;
@@ -38,17 +37,58 @@ public class PatientControllerTest {
     private PatientService patientService;
 
     @Test
-    public void givenValidRequest_whenCreatePatient_thenShouldOk() throws Exception {
-        SavePatientRequest request = SavePatientRequest.builder().name("mucahit").surname("bayar").build();
+    public void getGivenValidRequestParameters_WhenFindOnePatient_thenReturnResource() throws Exception {
+        given(this.patientService.findOnePatient(1)).willReturn(preparePatientUser("mucahit", "bayar"));
 
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.configure(SerializationFeature.WRAP_ROOT_VALUE, false);
-
-        ObjectWriter ow = (ObjectWriter) mapper.writer().withDefaultPrettyPrinter();
-        String requestJson=ow.writeValueAsString(anObject );
-
-
-        this.mockMvc.perform(post("/create").contentType(APPLICATION_JSON_UTF8).content(request)).andDo(print()).andExpect(status().isOk());
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/api/patient/one").param("id", "1")
+                        .accept(MediaType.APPLICATION_JSON)).andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value("mucahit"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.surname").value("bayar"));
     }
 
+    @Test
+    public void givenValidRequestParameters_whenCreatePatient_thenShouldOk() throws Exception {
+        SavePatientRequest request = SavePatientRequest.builder().name("mucahit").surname("bayar").build();
+
+        this.mockMvc.perform(MockMvcRequestBuilders.post("/api/patient/create")
+                        .content(new ObjectMapper().writeValueAsString(request)).contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void givenValidRequestParameters_whenListPatient_thenShouldOk() throws Exception {
+
+        given(this.patientService
+                .getAllPatientUsers())
+                .willReturn(Collections
+                        .singletonList(preparePatientUser("mucahit", "bayar")));
+
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/api/patient/list")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].name").value("mucahit"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].surname").value("bayar"));
+    }
+
+    @Test
+    void givenValidRequestParameters_whenUpdatePatient_thenReturnResource() throws Exception {
+        UpdatePatientRequest request = UpdatePatientRequest.builder().name("mucahit").surname("bayar").build();
+
+        this.mockMvc.perform(MockMvcRequestBuilders.
+                put("/api/patient/update")
+                .content(new ObjectMapper().writeValueAsString(request))
+                .contentType(MediaType.APPLICATION_JSON)).andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    private PatientUser preparePatientUser(String name, String surname) {
+        PatientUser user = new PatientUser();
+        user.setName(name);
+        user.setSurname(surname);
+
+        return user;
+    }
 }
